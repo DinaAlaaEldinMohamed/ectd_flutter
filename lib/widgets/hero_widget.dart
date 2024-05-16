86bdc1351a5a536ec137f5245bbe4ea7ee6bdc6d
dandash <dina.3laa93@gmail.com>
2024-05-17 01:15:18 +0300

    make hero widget reusable

--- lib/widgets/hero_widget.dart
+++ lib/widgets/hero_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HeroWidget extends StatelessWidget {
   String tag;
@@ -13,7 +14,7 @@ class HeroWidget extends StatelessWidget {
         return Container(
           width: heroSize.width,
           height: heroSize.height,
-          color: Colors.red, // لون البديل
+          color: const Color.fromARGB(255, 217, 214, 213), // لون البديل
         );
       },
       child: Container(
